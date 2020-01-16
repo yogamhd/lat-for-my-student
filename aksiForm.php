0f5f39479617b1f51e223952d138ba532b021937
yogamhd <yogapratamamhd@gmail.com>
2020-01-17 00:07:17 +0700

    add profile with css

--- aksiForm.php
+++
@@ -1,9 +0,0 @@
-<?php
- $nama = $_POST['nama'];
- $umur = $_POST['umur'];
- $alamat = $_POST['alamat'];
-
-echo "Selamat $nama , Biodata anda berhasil ditambahkan<br><br>";
-echo "nama = $nama <br>umur = $umur <br>alamat = $alamat";
-
- ?>
